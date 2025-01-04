179931b27910a11077d759b6cb186cf3bbcdd8b1
girlCoder8 <62305157+girlCoder8@users.noreply.github.com>
2025-01-03 22:32:23 -0500

    init commit

--- app_chat.py
+++ app_chat.py
@@ -8,7 +8,6 @@ from dotenv import load_dotenv
 load_dotenv()
 GOOGLE_API_KEY = os.environ.get('GOOGLE_API_KEY')
 genai.configure(api_key=GOOGLE_API_KEY)
-# genai.configure(api_key=GOOGLE_API_KEY)
 
 new_chat_id = f'{time.time()}'
 MODEL_ROLE = 'ai'
